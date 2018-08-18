5c19c7071aa16bcd5c7ee758037e592b0f8b716a
iman nemati <imun22@gmmail.com>
2018-08-18 19:45:07 +0430

    Initial commit

---
+++ src/SharpInterview/SharpInterview/Algorithms/ArrayQuestion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SharpInterview.Algorithms {
+    public static class ArrayQuestion {
+        public static double Average(params int[] input) {
+            if (input == null || input.Length == 0)
+                return double.NaN;
+            int sum = 0;
+            for (var i = 0; i <= input.Length; i++) {
+                sum += input[i];
+            }
+            double result = (double)sum / input.Length;
+            return result;
+        }
+    }
+}
---
+++ src/SharpInterview/SharpInterview/Algorithms/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿namespace SharpInterview.Algorithms {
+    public static class BinarySearch {
+        public static int BinarySearchRecursive(int[] input, int key, int min, int max) {
+            if (min > max) {
+                return -1;
+            }
+            else {
+                int mid = (min + max) / 2;
+                if (key == input[mid]) {
+                    return ++mid;
+                }
+                else if (key < input[mid]) {
+                    return BinarySearchRecursive(input, key, min, mid - 1);
+                }
+                else {
+                    return BinarySearchRecursive(input, key, mid + 1, max);
+                }
+            }
+        }
+
+        public static int BinarySearchNormal(int[] input, int key, int min, int max) {
+            while (min <= max) {
+                int mid = (min + max) / 2;
+                if (key == input[mid]) {
+                    return ++mid;
+                }
+                else if (key < input[mid]) {
+                    max = mid - 1;
+                }
+                else {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
